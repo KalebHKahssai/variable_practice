94d595ed734aaa16ccdac4942798645b17bccfc2
Kaleb Kahssai <kalebkahssai@gmail.com>
2022-08-27 18:45:14 -0700

    Show reassigned values

--- variables.rb
+++ variables.rb
@@ -1,11 +1,22 @@
 email_personal = "helloworld@gmail.com"
 email_school = "student@turing.edu"
 
+email_personal = "ohboy@gmail.com"
+email_school = "student@ucla.edu"
+puts email_personal
+puts email_school
+
 average_age_man = 33
 average_age_woman = 24
+puts average_age_man
+puts average_age_woman
 
 amount_paid = 1200.00
 amount_owed = 18800.00
+puts amount_paid
+puts amount_owed
 
 deposit_paid = true
 tution_paid = false
+puts deposit_paid
+puts tution_paid
